0906eda2914874b331023642926be09e3abd163d
Syed-Sherjeel <sherjeelhashmi@gmail.com>
2023-06-05 10:37:12 +0500

    feat: adds logo for static file

--- Job_Post_Application/backend/main.py
+++ Job_Post_Application/backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
@@ -7,9 +8,14 @@ def include_router(application: FastAPI):
     application.include_router(general_pages_router)
 
 
+def configure_static(application: FastAPI):
+    application.mount("/static", StaticFiles(directory="static"), name="static")
+
+
 def start_application():
     application = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(application)
+    configure_static(application)
     return application
 
 
