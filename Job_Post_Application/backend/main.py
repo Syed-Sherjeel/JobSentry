9c2fa21beb3c64939a2eb4cf6a5d7e2b5412fbb8
ssherjeel <ssherjeel@xisys.com>
2023-06-05 10:02:41 +0500

    feat: adds html serving with fastapi

--- Job_Post_Application/backend/main.py
+++ Job_Post_Application/backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+
+def include_router(application: FastAPI):
+    application.include_router(general_pages_router)
 
 
-@app.get("/")
-def hello_api():
-    return {"msg": "Hello API"}
+def start_application():
+    application = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(application)
+    return application
+
+
+app = start_application()
+
