de24e77a604d0c89f1c94af79dab55416e7386d5
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-08 14:01:43 +0500

    feat: adds job creation route

--- Job_Post_Application/apis/__init__.py
+++ Job_Post_Application/apis/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .version_1 import route_general_page
 from .version_1 import route_users
+from .version_1 import route_jobs
 
 api_router = APIRouter()
 api_router.include_router(route_general_page.general_pages_router, prefix="", tags=["general_pages"])
 api_router.include_router(route_users.router, prefix="/users", tags=["users"])
+api_router.include_router(route_jobs.router, prefix="/jobs", tags=["jobs"])
 
 __all__ = ["api_router"]
