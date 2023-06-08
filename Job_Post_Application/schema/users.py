b0d40c369f7a2392acaa3e0a04d9c540e9565392
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-08 12:42:09 +0500

    refact: restructres code

---
+++ Job_Post_Application/schema/users.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+# fields required during user creation
+
+class CreateUser(BaseModel):
+    username: str 
+    password: str
+    email: EmailStr
+
+
+class ShowUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+    
+    class Config():
+        orm_mode = True
+        
