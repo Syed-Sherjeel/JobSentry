08a3cb87375cb0835ede06c25f0f673dab04c92c
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-17 11:46:30 +0500

    feat: adds reference of create jwt token in module

--- Job_Post_Application/core/__init__.py
+++ Job_Post_Application/core/__init__.py
@@ -1,4 +1,5 @@
 from .config import settings
 from .hashing import Hasher
+from .security import create_access_token
 
-__all__ = ["settings", "Hasher"]
+__all__ = ["settings", "Hasher", "create_access_token"]
