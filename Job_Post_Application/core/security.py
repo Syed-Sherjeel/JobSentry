19a03499b3c14e5b422d1bc4efec90a475fb418d
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-17 11:47:04 +0500

    fix: fixes issue that caused token not being loaded as string

--- Job_Post_Application/core/security.py
+++ Job_Post_Application/core/security.py
@@ -16,6 +16,11 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
         )
     to_encode.update({"exp": expire})
     encode_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+        to_encode, str(settings.SECRET_KEY), algorithm=settings.ALGORITHM
     )
     return encode_jwt
+
+
+if __name__ == "__main__":
+    data = {"sub": "hunterfisher@aol.com"}
+    print(create_access_token(data))
