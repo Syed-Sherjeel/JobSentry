eb28b91b94accc4e63f7f679877153e6fb729a24
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-17 12:56:34 +0500

    fix: catches exception which occurrs when email that is already being used is specified in creata-user api

--- Job_Post_Application/apis/version_1/route_users.py
+++ Job_Post_Application/apis/version_1/route_users.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from fastapi import Depends
+from sqlalchemy.exc import IntegrityError
+from fastapi import Depends, status, HTTPException
 
 from schema.users import CreateUser, ShowUser
 from db.session import get_db
@@ -11,5 +12,11 @@ router = APIRouter()
 
 @router.post("/create-user/", response_model=ShowUser)
 def create_user(user: CreateUser, db: Session = Depends(get_db)):
-    user = create_new_user(user=user, db=db)
+    try:
+        user = create_new_user(user=user, db=db)
+    except IntegrityError:
+        raise HTTPException(
+            detail="Username or Email already exists",
+            status_code=status.HTTP_409_CONFLICT,
+        )
     return user
