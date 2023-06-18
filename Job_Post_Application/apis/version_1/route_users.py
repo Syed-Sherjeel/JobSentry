5d8a46380ab9375870215695bef3c2f1a5431d30
Syed Sherjeel <sherjeelhashmi@gmail.com>
2023-06-18 11:57:42 +0500

    fix: adds fix that prevents user from being created if it already exists

--- Job_Post_Application/apis/version_1/route_users.py
+++ Job_Post_Application/apis/version_1/route_users.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import IntegrityError
 from fastapi import Depends, status, HTTPException
 
 from schema.users import CreateUser, ShowUser
 from db.session import get_db
-from db.repository.users import create_new_user
+from db.repository.users import create_new_user, get_user_by_email
 
 router = APIRouter()
 
 
 @router.post("/create-user/", response_model=ShowUser)
 def create_user(user: CreateUser, db: Session = Depends(get_db)):
-    try:
-        user = create_new_user(user=user, db=db)
-    except IntegrityError:
+    user_exists = get_user_by_email(user.email, db)
+    if user_exists:
         raise HTTPException(
             detail="Username or Email already exists",
             status_code=status.HTTP_409_CONFLICT,
         )
+    user = create_new_user(user=user, db=db)
     return user
