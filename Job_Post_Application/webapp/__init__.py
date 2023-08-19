0a62d31b2cd4808fb7940d3be15514956a4dbd34
Vyro <vyro@Vyros-MacBook-Pro.local>
2023-08-19 22:41:21 +0500

    feat: adds user login api with frontend

--- Job_Post_Application/webapp/__init__.py
+++ Job_Post_Application/webapp/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .jobs import route_jobs
 from .users import route_user
+from .auth import route_login
 
 web_page_router = APIRouter()
 web_page_router.include_router(route_jobs.router, prefix="", tags=["jobs-webapp"])
 web_page_router.include_router(route_user.router, prefix="", tags=["users-webapp"])
+web_page_router.include_router(route_login.router, prefix="", tags=["user-auth"])
 
 __all__ = ["web_page_router"]
