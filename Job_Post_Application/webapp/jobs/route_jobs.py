977ef047c01a2657d04f81a51d3f91b46ed621cc
Vyro <vyro@Vyros-MacBook-Pro-2.local>
2023-08-26 14:20:48 +0500

    feat: adds job create route and webpage

--- Job_Post_Application/webapp/jobs/route_jobs.py
+++ Job_Post_Application/webapp/jobs/route_jobs.py
@@ -1,10 +1,19 @@
 from fastapi import APIRouter
+from fastapi import APIRouter
 from fastapi import Request, Depends
+from fastapi import status, responses
 from fastapi.templating import Jinja2Templates
+
 from sqlalchemy.orm import Session
 
 from db.session import get_db
+from db.model.users import User
+from schemas.jobs import JobCreate
+from webapps.jobs.forms import JobCreateForm
+from db.repository.jobs import create_new_job
 from db.repository.jobs import list_jobs, retrieve_job
+from apis.version1.route_login import get_current_user_from token
+
 
 router = APIRouter(include_in_schema=False)
 templates = Jinja2Templates(directory="templates")
@@ -24,3 +33,29 @@ async def job_details(request: Request, record_id: int, db: Session = Depends(ge
     return templates.TemplateResponse(
         "jobs/details.html", {"request": request, "job": job}
     )
+
+@router.get("/post-a-job/")
+def create_job(requests: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("jobs/create_job.html", {"request": requests})
+
+@router.post("/post-a-job/")
+def create_job(request: Request,
+               db: Session = Depends(get_db),
+               current_user: User = Depends):
+    form = JobCreateForm(request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            job = JobCreate(**form.__dict__)
+            job = create_new_job(job=job, db=db, owner_id=current_user.id)
+            return responses.RedirectResponse(
+                f"/details/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in, In case problem persists please contact us"
+            )
+
+
+    return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
