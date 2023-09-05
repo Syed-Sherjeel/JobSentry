87c611a3bc898f744d8f6c8799dcf8ef55658802
SyedSherjeel <sherjeelhashmi@gmail.com>
2023-09-05 23:55:10 +0500

    feat: adds load testing with locust

--- Job_Post_Application/db/session.py
+++ Job_Post_Application/db/session.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from core import settings
 
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_size=20, max_overflow=100)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
