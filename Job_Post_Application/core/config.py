d634960baa7041500bbc9e8d8160a162f88f684a
SyedSherjeel <sherjeelhashmi@gmail.com>
2023-09-05 22:00:27 +0500

    feat: adds password as config file

--- Job_Post_Application/core/config.py
+++ Job_Post_Application/core/config.py
@@ -1,25 +1,33 @@
 import os
 
 
-from pathlib import Path
-from dotenv import load_dotenv
+import yaml
+config_path = "config.yml"
 
-env_path = Path(".") / ".env"
-load_dotenv(dotenv_path=env_path)
+
+def load_db_config():
+    if not os.path.exists(config_path):
+        raise FileNotFoundError("Credentials not found")
+    with open(config_path, 'r') as config_file:
+        config = yaml.safe_load(config_file)
+    return config
+
+
+config = load_db_config()
 
 
 class Settings:
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_USER: str = config["database"]["POSTGRES_USER"]
+    POSTGRES_PASSWORD = config["database"]["POSTGRES_PASSWORD"]
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT: str = os.getenv(
         "POSTGRES_PORT", 5432
     )  # default postgres port is 5432
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    SECRET_KEY: str = config["database"]["SECRET_KEY"]
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
